2e99a6dff24b6f8f58342015d2e6c2c635169eea
Prolland006 <rolland.patrice@gmail.com>
2016-05-05 15:50:44 +0200

    release 1.0.2

--- checkvpn/CheckVpn.java
+++ checkvpn/CheckVpn.java
@@ -17,6 +17,7 @@ import java.io.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JCheckBox;
 import javax.swing.GroupLayout;
 import java.awt.Color;
 import java.net.URL;
@@ -24,21 +25,39 @@ import java.io.Serializable;
 import java.util.StringTokenizer;
 import CheckVpn.common.SplashWindowFrame;
 import java.awt.ComponentOrientation;
+import java.lang.Boolean;
 
 /////////////////////////////////////////////////////////////////////////////////
 public class CheckVpn {
     
-static final String version = "1.0.1";
+static final String version = "1.0.2";
 
 SplashWindowFrame sp;
-JTextArea informationArea;
-JTextField textFieldIp;
-JTextField textFieldSoft;
 JFrame frame;
-JLabel jlbStatus;
-JPanel statusPanel;
-JPanel ipPanel;
-JButton jbUpdateIp;
+JPanel jp1=new JPanel();
+JPanel jp2=new JPanel();
+JPanel jp3=new JPanel();
+JPanel ipPanel=new JPanel();
+JPanel jpnCenter=new JPanel();
+JPanel panel = new JPanel();
+JLabel labelIp= new JLabel("Your External Ip without VPN");
+JLabel jlbGap=new JLabel("Checking gap in seconds");
+JLabel jlbURL=new JLabel("URL to get your ip");
+JLabel jlbAutoStart=new JLabel("Auto start");
+JPanel panelNorth = new JPanel();
+JLabel jlbStatus = new JLabel("VPN KO");
+JPanel statusPanel = new JPanel();
+JLabel labelSoft= new JLabel("Software to stop when no VPN");
+JButton startButton=new JButton("START");
+JButton jbValidSoft=new JButton("Save settings");
+JButton jbUpdateIp=new JButton("Update Ip");
+JScrollPane informationScrollPane = new JScrollPane();
+JTextArea informationArea = new JTextArea();
+JTextField textFieldSoft= new JTextField();
+JTextField textFieldGap=new JTextField();
+JTextField textFieldIp= new JTextField();
+JTextField textFieldURL=new JTextField();
+JCheckBox jcbAutoStart=new JCheckBox();
 
 Timer timer;
 Timer timerSplashScreen;
@@ -49,6 +68,9 @@ CInfoVPN infoVPN;
     public class CInfoVPN implements Serializable {
         String stIp;
         String stSoftware;
+        String stGap;
+        String stURL;
+        Boolean bAutoStart;
         
         public void serialize() {
            try {
@@ -60,6 +82,9 @@ CInfoVPN infoVPN;
 
              oos.writeObject(stIp);
              oos.writeObject(stSoftware);
+             oos.writeObject(stGap);
+             oos.writeObject(stURL);
+             oos.writeObject(bAutoStart);
              oos.flush();
              oos.close();
            } catch (NotSerializableException nse){System.out.println(nse);}
@@ -75,28 +100,32 @@ CInfoVPN infoVPN;
           public boolean deserialize (String file) {
             boolean rt=true;
             try {
-              ObjectInputStream ois = new ObjectInputStream(
+                ObjectInputStream ois = new ObjectInputStream(
                                   new BufferedInputStream(
                                           new FileInputStream(file)));
-              this.stIp=(String)(ois.readObject());
-              this.stSoftware=(String)(ois.readObject());
-              ois.close();
+                this.stIp=(String)(ois.readObject());
+                this.stSoftware=(String)(ois.readObject());
+                this.stGap=(String)(ois.readObject());
+                this.stURL=(String)(ois.readObject());
+                this.bAutoStart=(Boolean)(ois.readObject());
+                ois.close();
             } catch (ClassNotFoundException cnfe) {
-              System.out.println(cnfe);
-              rt=false;
+                System.out.println(cnfe);
+                rt=false;
             } catch (FileNotFoundException fnfe) {
-              System.out.println(fnfe);
-              rt=false;
-                  } catch (IOException ioe) {
-                    System.out.println(ioe);
-              rt=false;
+                System.out.println(fnfe);
+                rt=false;
+            } catch (IOException ioe) {
+                System.out.println(ioe);
+                rt=false;
             }
+            System.out.println(rt);
             return rt;
           }        
     }
 
-    public static String getMyExternalIp() throws Exception {
-        URL whatismyip = new URL("http://checkip.amazonaws.com");
+    public static String getMyExternalIp(String url) throws Exception {
+        URL whatismyip = new URL(url); //"http://checkip.amazonaws.com"
         BufferedReader in = null;
         try {
             in = new BufferedReader(new InputStreamReader(
@@ -129,7 +158,7 @@ CInfoVPN infoVPN;
       
     // Création d'un timer qui génère un tic
     // chaque 500 millième de seconde
-    return new Timer (2000, action);
+    return new Timer (Integer.parseInt(this.textFieldGap.getText())*1000, action);
   }      
 
 // Méthode renvoyant un timer prêt à démarrer
@@ -161,22 +190,44 @@ CInfoVPN infoVPN;
   
   void updateIp_actionPerformed(ActionEvent e) {
     try {
-        this.textFieldIp.setText(CheckVpn.getMyExternalIp());
+        this.textFieldIp.setText(CheckVpn.getMyExternalIp(this.textFieldURL.getText()));
     } catch (Exception exc) {
         exc.printStackTrace();
+        this.trace(exc.toString());
     }
   }
   
   void save_actionPerformed(ActionEvent e) {
-      this.infoVPN.stIp=this.textFieldIp.getText();
-      this.infoVPN.stSoftware=this.textFieldSoft.getText();
-      this.infoVPN.serialize();
+        this.infoVPN.stIp=this.textFieldIp.getText();
+        this.infoVPN.stSoftware=this.textFieldSoft.getText();
+        this.infoVPN.stGap=this.textFieldGap.getText();
+        this.infoVPN.stURL=this.textFieldURL.getText();
+        this.infoVPN.bAutoStart=Boolean.valueOf(this.jcbAutoStart.isSelected());
+        this.infoVPN.serialize();
   }
-    
+  
+  void start() {
+        startButton.setText("STOP");
+        System.out.println("Start the timer delay "+this.textFieldGap.getText());
+        timer.setDelay(Integer.parseInt(this.textFieldGap.getText())*1000);
+        timer.start();      
+  }
+  
+  void start_actionPerformed(ActionEvent e) {
+        if (timer.isRunning()) {
+            startButton.setText("START");
+            System.out.println("Stop the timer...");
+            vpnStopped();
+            timer.stop();
+        } else {
+            start();
+        }
+  }
+  
   void Timer_actionPerformed(ActionEvent e) {
         String tok;
         try {
-            String st=CheckVpn.getMyExternalIp();
+            String st=CheckVpn.getMyExternalIp(this.textFieldURL.getText());
 
             trace("external ip="+st);
                     
@@ -211,70 +262,51 @@ CInfoVPN infoVPN;
     String[] st={"Release "+CheckVpn.version,"GNU General Public License version 3.0 (GPLv3)", "Author Patrice Rolland"};
     sp = new SplashWindowFrame("CheckVpn/pic/logo.jpg", st);
     createTimerSplashScreen ().start();
-    
-    infoVPN=new CInfoVPN();
-    infoVPN.deserialize();
-      
-    this.timer=createTimer();
 
+    //read serialized data
+    infoVPN=new CInfoVPN();
+    if (!infoVPN.deserialize()) {
+        initDefaultValue();
+    } else {
+        textFieldIp.setText(this.infoVPN.stIp);
+        textFieldGap.setText(this.infoVPN.stGap);
+        textFieldURL.setText(infoVPN.stURL);
+        textFieldSoft.setText(this.infoVPN.stSoftware);
+        this.jcbAutoStart.setSelected(this.infoVPN.bAutoStart.booleanValue());
+    }
+      
     //Create internal frame
     frame = new JFrame("CHECK VPN - Release "+CheckVpn.version);
     frame.setSize( 325, 200);
     frame.setLocation( 50, 50);
     frame.setVisible(false);
     
-    JPanel ipPanel=new JPanel();
-    JPanel jpnCenter=new JPanel();
-    JButton startButton=new JButton("START");
-    startButton.addActionListener(new java.awt.event.ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        if (timer.isRunning()) {
-            startButton.setText("START");
-            System.out.println("Stop the timer...");
-            vpnStopped();
-            timer.stop();
-        } else {
-            startButton.setText("STOP");
-            System.out.println("Start the timer...");
-            timer.start();
-        }
-      }
-    });    
+
+    startButton.addActionListener(
+        new ActionListener() {public void actionPerformed(ActionEvent e) {start_actionPerformed(e);}}
+    );    
     
     startButton.setMaximumSize(new Dimension(100,44));
     startButton.setPreferredSize(new Dimension(100,44));
     startButton.setMinimumSize(new Dimension(100,44));
     jpnCenter.add(startButton);
-     
-    JLabel labelIp= new JLabel("Your External Ip without VPN");
+        
     labelIp.setPreferredSize(new Dimension(100, 22));
     labelIp.setMaximumSize(new Dimension(100, 22));
 
-    textFieldIp= new JTextField("999.999.999.999");
     textFieldIp.setPreferredSize(new Dimension(100, 22));
-    textFieldIp.setMaximumSize(new Dimension(100, 22));
-    textFieldIp.setText(this.infoVPN.stIp);
+    textFieldIp.setMaximumSize(new Dimension(100, 22));   
     
-
-   
-    JLabel labelSoft= new JLabel("Software to stop when no VPN");
-
-    textFieldSoft= new JTextField("firefow.exe;utorrent.exe");
-    textFieldSoft.setText(this.infoVPN.stSoftware);
     
-    JButton jbUpdateIp=new JButton("Update Ip");
     jbUpdateIp.addActionListener(
       new ActionListener() {public void actionPerformed(ActionEvent e) {updateIp_actionPerformed(e);}}
     );    
 
-    JButton jbValidSoft=new JButton("Save settings");
     jbValidSoft.addActionListener(
       new ActionListener() {public void actionPerformed(ActionEvent e) {save_actionPerformed(e);}}
     );    
 
 
-    jlbStatus = new JLabel("VPN KO");
-    statusPanel = new JPanel();
     statusPanel.add(jlbStatus);
     statusPanel.setPreferredSize(new Dimension(100, 22));
     statusPanel.setMaximumSize(new Dimension(100, 22));
@@ -285,10 +317,7 @@ CInfoVPN infoVPN;
     ipPanel.add(textFieldIp);
     ipPanel.add(jbUpdateIp);
     ipPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-    
-    vpnStopped();
-
-    JPanel panelNorth = new JPanel();
+  
     GroupLayout gpLayout=new GroupLayout(panelNorth);
     panelNorth.setLayout(gpLayout);
     gpLayout.setAutoCreateGaps(true);
@@ -296,12 +325,21 @@ CInfoVPN infoVPN;
     gpLayout.setHorizontalGroup(gpLayout.createSequentialGroup()
         .addGroup(gpLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addComponent(labelIp)
-            .addComponent(labelSoft))
+            .addComponent(labelSoft)
+            .addComponent(jlbGap)
+            .addComponent(jlbURL)
+            .addComponent(jlbAutoStart))
         .addGroup(gpLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addComponent(ipPanel)
-            .addComponent(textFieldSoft))
+            .addComponent(textFieldSoft)
+            .addComponent(textFieldGap)
+            .addComponent(textFieldURL)
+            .addComponent(jcbAutoStart))
         .addGroup(gpLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addComponent(statusPanel)
+            .addComponent(jp1)
+            .addComponent(jp2)        
+            .addComponent(jp3)
             .addComponent(jbValidSoft))
     );    
     gpLayout.setVerticalGroup(gpLayout.createSequentialGroup()
@@ -312,18 +350,26 @@ CInfoVPN infoVPN;
         .addGroup(gpLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
             .addComponent(labelSoft)
             .addComponent(textFieldSoft)
+            .addComponent(jp1))
+        .addGroup(gpLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+            .addComponent(jlbGap)
+            .addComponent(textFieldGap)
+            .addComponent(jp2))
+        .addGroup(gpLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+            .addComponent(jlbURL)
+            .addComponent(textFieldURL)
+            .addComponent(jp3))
+        .addGroup(gpLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+            .addComponent(jlbAutoStart)
+            .addComponent(jcbAutoStart)
             .addComponent(jbValidSoft))
     );
 
-
-    
-    JPanel panel = new JPanel();
+   
     panel.setLayout(new BorderLayout());
  
-    informationArea = new JTextArea();
     informationArea.setEditable(false);
                 
-    JScrollPane informationScrollPane = new JScrollPane();
     informationScrollPane.getViewport().add(informationArea, null);
 
     panel.add(informationScrollPane);
@@ -338,9 +384,13 @@ CInfoVPN infoVPN;
    frame.addWindowListener(
       new WindowAdapter() {public void windowClosing(WindowEvent e) {System.exit(0);}}
     );
-    frame.setSize( 500, 300);
+    frame.setSize( 500, 400);
     
-    
+    this.timer=createTimer();
+    if (this.jcbAutoStart.isSelected())
+        start();
+    else vpnStopped();
+  
    }
 
     public void vpnOK(String st) {
@@ -372,6 +422,20 @@ CInfoVPN infoVPN;
     informationArea.setCaretPosition(entireString.length());
     frame.validate();
     frame.repaint();
+  }
+  
+  public void initDefaultValue() {
+      System.out.println("initDefaultValue");
+      try {
+        this.textFieldGap.setText("2");
+        this.textFieldIp.setText(CheckVpn.getMyExternalIp("http://checkip.amazonaws.com"));
+        this.textFieldURL.setText("http://checkip.amazonaws.com");
+        this.textFieldSoft.setText("utorrent.exe;firefox.exe");
+        this.jcbAutoStart.setSelected(false);
+      } catch (Exception exc) {
+          exc.printStackTrace();
+          this.trace(exc.toString());
+      }
   }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-CheckVPN Documentation 1,0,1
+﻿CheckVPN Documentation 1,0,1
 ============================
 Summary
 -------
@@ -10,6 +10,8 @@ Setting
 - Double click on it
 - Click on the button « Update ip » to get your external ip address.
 - Type the programs separated by semicolon that you would like to stop when the vpn is falling down (ex: utorrent.exe;firefox.exe)
-- Click on start button and the program will check every 2 seconds your ip address, if it not the vpn's ip the programs will be stopped by this tool.
+- Click on start button and the program will check every x seconds your ip address, if it not the vpn's ip the programs will be stopped by this tool.
+- The option « auto start » start the process automaticaly when you start the tool without clicking on the button « start ».
+- If you remove the .ser file you will have the default parameters.
 
 the web site [http://check.vpn.free.fr](http://check.vpn.free.fr)
